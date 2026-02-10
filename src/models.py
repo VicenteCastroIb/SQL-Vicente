4df6279adb597b98cd715f1e1dd18e9e03498cac
Vicente Castro Ibarra <vicentecastroibarra@gmail.com>
2026-02-10 22:21:55 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -1,19 +1,110 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, DateTime, func, ForeignKey
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    full_name: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    profile_picture: Mapped[str] = mapped_column(String(250), nullable=True)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(), default=func.now())
 
+    posts = relationship("Post", backref="user")
+    comments = relationship("Comment", backref="user")
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "full_name": self.full_name,
             "email": self.email,
+            "profile_picture": self.profile_picture,
+            "is_active": self.is_active,
+            "created_at": self.created_at.isoformat() if self.created_at else None
+
             # do not serialize the password, its a security breach
         }
+
+
+class Post(db.Model):
+    __tablename__ = "post"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    image_url: Mapped[str] = mapped_column(String(120), nullable=False)
+    caption: Mapped[str] = mapped_column(String(2200), nullable=False)
+    location: Mapped[str] = mapped_column(String(1000))
+    created_at: Mapped[datetime] = mapped_column(DateTime(), default=func.now())
+
+    comments = relationship("Comment", backref="post")
+    media = relationship("Media", backref="post")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "image_url": self.image_url,
+            "caption": self.caption,
+            "location": self.location,
+            "created_at": self.created_at.isoformat() if self.created_at else None
+
+        }
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(2200))
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(), default=func.now())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text,
+            "author_id": self.author_id,
+            "post_id": self.post_id,
+            "created_at": self.created_at.isoformat() if self.created_at else None
+        }
+
+class Media(db.Model):
+    __tablename__ = "media"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(), default=func.now())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id,
+            "created_at": self.created_at.isoformat() if self.created_at else None
+        }
+    
+class Follower(db.Model):
+    __tablename__ = "follower"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+
+# Profe, no podia hacer correr bien el diagrama, asi que busque info y añadi estas lineas, logre correr el diagrama
+# con este comando: pipenv run python src/models.py
+import sys
+from eralchemy2 import render_er
+
+
+try:
+    render_er(db.Model, 'diagram.png')
+    print("✅ ¡Éxito! El diagrama 'diagram.png' ha sido actualizado.")
+except Exception as e:
+    print("❌ Hubo un error al generar el diagrama")
+    print(e)
